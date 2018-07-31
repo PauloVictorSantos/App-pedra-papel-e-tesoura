78448542490c1a84f28b8b07f8c0cb2c3931a15d
paulovictorln <paulovictor494@gmail.com>
2018-07-30 23:47:37 -0300

    state

--- index.android.js
+++ index.android.js
@@ -13,25 +13,38 @@ import {
 } from 'react-native';
 
 class MeuComponente extends Component{
+
+constructor(props)
+{
+  super(props);
+  this.state ={texto:'Testando'}
+}
+
 render(){
   return(
     <View style={styles.container}>
-      <Text>{this.props.propried}</Text>
-      <Text>{this.props.zyx}</Text>
+      <Text>{this.props.teste}</Text>
     </View>
   );
 }
+alteraTexto() {
+  this.setState({texto:'Outra coisa'})
+}
 
 }
+
 
 export default class app3 extends Component {
   render() {
     return (
-        <MeuComponente propried='banana' zyx='teste'></MeuComponente>
-
+      <View>
+        <MeuComponente teste={this.state.texto}></MeuComponente>
+          <button onPress={ ()=>alteraTexto} title="Botão"  />
+        </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
