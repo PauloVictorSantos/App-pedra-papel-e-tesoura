b5e4cc11ff13c6fc131a17b538395f797cfa8ed0
paulovictorln <paulovictor494@gmail.com>
2018-07-31 22:15:16 -0300

    logica do jogo

--- index.android.js
+++ index.android.js
@@ -13,36 +13,38 @@ import {
   Button
 } from 'react-native';
 
-
-class MeuComponente extends Component{
-  render(){
-    return(
-      <View style={styles.container}>
-        <Text>{this.props.teste}</Text>
-      </View>
-    );
+export default class app3 extends Component {
+  constructor(props){
+    super(props);
+    this.state ={escolhaUsuario:'', escolhaComputador:''};
   }
+  jokenpo(escolhaUsuario){
+    //gera número aleatorio(0,1,2)
+    var numAleatorio =Math.floor(Math.random() * 3);
+    var escolhaComputador ='';
+switch(numAleatorio){
+  case 0: escolhaComputador ='pedra';
+  break;
+  case 1: escolhaComputador = 'papel';
+  break;
+  case 2: escolhaComputador = 'tesoura';
+  break;
 }
 
-
-export default class app3 extends Component {
-
-  constructor(props)
-  {
-    super(props);
-    this.state ={texto:'Testando'};
+    this.setState({escolhaUsuario: escolhaUsuario, escolhaComputador:escolhaComputador});
   }
-  
-    alteraTexto() {
-      this.setState({texto:'Outra coisa'});
-    }
 
   render() {
     return (
       <View>
-        <MeuComponente teste={this.state.texto}></MeuComponente>
-          <Button onPress={()=>{this.alteraTexto()}} title="Botão"  />
-        </View>
+        <Text>Escolha do computador {this.state.escolhaComputador}</Text>
+        <Text>Escolha do usuário {this.state.escolhaUsuario}</Text>
+        <Text>Resultado</Text>
+        <Button title="pedra" onPress={()=>{this.jokenpo('pedra')}}/>
+        <Button title="papel" onPress={()=>{this.jokenpo('papel')}}/>
+        <Button title="tesoura" onPress={()=>{this.jokenpo('tesoura')}}/>
+      </View>
+      
     );
   }
 }
